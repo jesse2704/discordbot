a787a21db7424dcaca4fc0923ec09c9950d75d98
Luuk de Haas <81307@b5-430-25.ict.lab.locals>
2018-09-27 11:50:42 +0200

    new version van kelly

--- kelly.py
+++ kelly.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-
+"""Author Luuk de Haas and Jesse Akoma"""
+"""Bot name Kelly""""
 TOKEN = 'NDkxNTA0NTg4NzQ3NDQwMTI4.DoI1RA.pzurKfgfNlcX94pbspDyzkl2lOU'
 
 description = '''ninjaBot in Python'''
@@ -22,6 +23,13 @@ async def hello():
     """Says world"""
     await bot.say("Hello there, how can I help you?")
 
+@bot.command()
+async def music(ctx):
+    await bot.say("https://www.youtube.com/watch?v=qU5FWU0SH0o  also https://www.youtube.com/watch?v=Rw-hw7rVOrA")
+
+@bot.command()
+async def website(ctx):
+    await bot.say("This is the Portfolio of Luuk de Haas https://81307.ict-lab.nl/Portfolio")
 
 @bot.command()
 async def add(left : int, right : int):
