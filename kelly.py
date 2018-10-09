d6ad75468b2494c1a4eed197393e7d963ff574c5
jesse2704 <jesse2704@gmail.com>
2018-10-09 10:00:38 +0200

    Versie ik weetniet meer we hebben hoofdpijn

--- kelly.py
+++ kelly.py
@@ -5,7 +5,7 @@ import asyncio
 
 
 """Author Luuk de Haas and Jesse Akoma"""
-"""Bot name Kelly""""
+
 TOKEN = 'NDkxNTA0NTg4NzQ3NDQwMTI4.DoI1RA.pzurKfgfNlcX94pbspDyzkl2lOU'
 
 description = '''ninjaBot in Python'''
@@ -27,18 +27,7 @@ async def ping(ctx):
     await bot.say ("Pong")
 print ("user has pinged.")
 
-@bot.command(pass.pass_context=True)
-async def info(ctx, user, discord.Member):
-await bot.say("The users name is: {}".format(user.name))
-await bot.say("The use ID is:".format(iser.id))
-await bot.say("The users status is:".format(user.status))
-await bot.say("The users highest rank is:".format(user.top_rule))
-await bot.say("The user joined at:".format(user.joined_at))
 
-@bot.command(pass_context=True)
-async def kick(ctx, user: discord_Member):
-await bot.say(":boot: Bye Bye I hope you enjoyed your stay".format(user.name))
-await bot.kick(user)
 
 @bot.command()
 async def hello():
@@ -60,8 +49,7 @@ async def info(ctx):
     #auteur
     embed.add_field(name="Author", value="Luuk de Haas")
     
-    #welke servers die zit
-    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
     
     #portfolio
     embed.add_field(name="Portfolio", value="https://81307.ict-lab.nl/Portfolio")
